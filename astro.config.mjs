1ff28c0ec8d0cecbd713bbfe940d50831bab49b1
Julien PAUSE <julien.pause@epitech.eu>
2023-12-22 18:09:22 +0400

    feat: add tailwind purge and astro compressor integration with gzip and brotli

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+import compressor from "astro-compressor";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ecolo-run.vercel.app/',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), compressor()]
 });
